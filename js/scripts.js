c24138e3e91fe69380098737d47fca1a7910d12d
Troy Delacruz <troy8076@yahoo.com>
2022-03-30 21:19:43 -0400

    Add clock

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,20 @@ function loop () {
 }
 
 loop();
+
+/* Creating time clock on navbar */
+
+const span = document.getElementById('clock');
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  span.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+
+setInterval(time, 1000);
+
+time();
